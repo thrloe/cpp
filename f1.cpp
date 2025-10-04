19317545d9572a609cdc25a8df804cbf83dc7703
thrloe <99206917+thrloe@users.noreply.github.com>
2025-10-04 11:39:11 +0300

    Update f1.cpp
    New way

--- f1.cpp
+++ f1.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
 
-void heapify(int arr[], int n, int i) {
-    int largest = i; // Инициализируем наибольший элемент как корень
-    int left = 2 * i + 1; // Левый дочерний элемент
-    int right = 2 * i + 2; // Правый дочерний элемент
-
-    // Если левый дочерний элемент больше корня
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
-    }
-
-    // Если правый дочерний элемент больше наибольшего элемента
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-
-    // Если наибольший элемент не корень
-    if (largest != i) {
-        std::swap(arr[i], arr[largest]); // Меняем местами
-
-        // Рекурсивно преобразуем затронутое поддерево в кучу
-        heapify(arr, n, largest);
-    }
-}
-
-void heapsort(int arr[], int n) {
-    // Построение кучи (перегруппировка массива)
-    for (int i = n / 2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-
-    // Один за другим извлекаем элементы из кучи
-    for (int i = n - 1; i >= 0; i--) {
-        std::swap(arr[0], arr[i]); // Перемещаем текущий корень в конец
-        heapify(arr, i, 0); // Вызываем heapify на уменьшенной куче
-    }
-}
+using namespace std;
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int day, hour, minute;
+    cin >> day >> hour >> minute;
+    int nooow = (day - 1) * 24 * 60 + hour * 60 + minute;
 
-    heapsort(arr, n);
+    int N;
+    cin >> N;
 
-    std::cout << "Отсортированный массив: \n";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+    int min_time = 1000000000;
+
+    for (int i = 0; i < N; i++) {
+        int d, h, m;
+        cin >> d >> h >> m;
+        int new_bud;
+
+        if (d == 0) {
+            for (int j = 0; j < 7; j++) {
+                new_bud = j * 24 * 60 + h * 60 + m;
+                if (new_bud < nooow) {
+                    new_bud += 7 * 24 * 60;
+                }
+                if (new_bud < min_time) {
+                    min_time = new_bud;
+                }
+            }
+        } else {
+            new_bud = (d - 1) * 24 * 60 + h * 60 + m;
+            if (new_bud < nooow) {
+                new_bud += 7 * 24 * 60;
+            }
+            if (new_bud < min_time) {
+                min_time = new_bud;
+            }
+        }
     }
-    std::cout << std::endl;
+
+    int total = min_time;
+    int result_d = (total / (24 * 60)) % 7 + 1;
+    int result_h = (total % (24 * 60)) / 60;
+    int result_m = total % 60;
+
+    cout << result_d << " " << result_h << " " << result_m << endl;
 
     return 0;
 }
